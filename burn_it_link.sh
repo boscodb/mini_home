b6302f57fa9ade108d01cd9d00c1c57ce50001cd
bzrq <bosco.dsouza.82@gmail.com>
2026-08-26 00:15:22 +0530

    Add sym linker: .tmux.conf

--- burn_it_link.sh
+++ burn_it_link.sh
@@ -1,3 +1,4 @@
 # ln -sfn /home/sage/data/zr/mini_home/config/i3 ~/.config/i3
 
 ln -sfn "$(realpath ./config/i3)" ~/.config/i3
+ln -sfn "$(realpath ./.tmux.conf)" ~/.tmux.conf # On a fresh machine/install, TPM will need to be installed manually. There is no benefit in 'vendoring' it in this repo.
