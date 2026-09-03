bbd1e0d85373d1ddf8d73691374c7bdb62886c26
bzrq <bosco.dsouza.82@gmail.com>
2026-09-03 16:51:53 +0530

    Add sym linker: nono config.

--- burn_it_link.sh
+++ burn_it_link.sh
@@ -1,6 +1,8 @@
 # ln -sfn /home/sage/data/zr/mini_home/config/i3 ~/.config/i3
 
-ln -sfn "$(realpath ./config/i3)" ~/.config/i3
-ln -sfn "$(realpath ./config/nvim)" ~/.config/nvim
+ln -sfn "$(realpath ./.config/i3)" ~/.config/i3
+ln -sfn "$(realpath ./.config/nvim)" ~/.config/nvim
 
 ln -sfn "$(realpath ./.tmux.conf)" ~/.tmux.conf # On a fresh machine/install, TPM will need to be installed manually. There is no benefit in 'vendoring' it in this repo.
+
+ln -sfn "$(realpath ./.config/nono)" ~/.config/nono
