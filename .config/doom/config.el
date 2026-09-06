20131542dd1b17ce9633cc72c5e500ecad4b4d8f
bzrq <bosco.dsouza.82@gmail.com>
2026-09-07 01:19:30 +0530

    need to merge this commit with an earlier one.

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -94,11 +94,8 @@
 ;; BEGIN: Common Lisp
 (after! sly
   (setq sly-lisp-implementations
-        '((sbcl ("ros" "-L" "sbcl-bin" "-Q" "run") :coding-system utf-8-unix)
         '((sbcl ("sbcl") :coding-system utf-8-unix)
-          (ccl  ("ros" "-L" "ccl-bin"  "-Q" "run"))
           (ccl  ("<path to ccl binary>"))
-          (ecl  ("ros" "-L" "ecl"      "-Q" "run") :coding-system utf-8-unix))
           (ecl  ("<path to ecl binary>") :coding-system utf-8-unix))
         sly-default-lisp 'sbcl))
 
