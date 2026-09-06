33e68935a0f2752459782b90da071fa62a867ba3
bzrq <bosco.dsouza.82@gmail.com>
2026-09-07 01:19:30 +0530

    Setup pynvim and update lazyvim.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+vim.g.python3_host_prog = vim.fn.expand("~/.local/share/nvim/python3/bin/python")
 -- vim.g.neomux_enable_tmux = 1
