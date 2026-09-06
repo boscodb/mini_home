616f0a53b6fb8d7a923a7c4d04534e244f8849ac
bzrq <bosco.dsouza.82@gmail.com>
2026-09-07 01:19:30 +0530

    Make sbcl available system-wide. Backed by `ros use <sbcl>`.

--- burn_it_link.sh
+++ burn_it_link.sh
@@ -1,12 +1,10 @@
-# ln -sfn /home/sage/data/zr/mini_home/config/i3 ~/.config/i3
-
 rm -rf ~/.config/i3
 ln -sfn "$(realpath ./.config/i3)" ~/.config/i3
 
 rm -rf ~/.config/nvim
 ln -sfn "$(realpath ./.config/nvim)" ~/.config/nvim
 
-rm ~/.tmux.conf
+rm -f ~/.tmux.conf
 ln -sfn "$(realpath ./.tmux.conf)" ~/.tmux.conf # On a fresh machine/install, TPM will need to be installed manually. There is no benefit in 'vendoring' it in this repo.
 
 rm ~/.config/nono
@@ -22,3 +20,6 @@ rm -rf ~/.config/doom
 ln -sfn "$(realpath ./.config/doom)" ~/.config/doom
 # ~/.config/emacs/bin/doom sync
 # ~/.config/emacs/bin/doom doctor
+
+rm -f ~/.local/bin/sbcl
+ln -sfn "$(realpath ./helpers/cl/roswell/sbcl)" ~/.local/bin/sbcl
